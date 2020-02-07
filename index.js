3c94adf1be3f23e75153d20360bb91be12a772fd
shuvajitgupta <shuvajitgupta123@gmail.com>
2020-02-07 18:14:12 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const path = require("path");
 const PORT = process.env.PORT || 81;
 
 app.use(express.static(path.join(__dirname, "index.html")));
