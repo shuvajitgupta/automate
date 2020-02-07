398074748cc53f9163b5e5a83aaee7469eebee96
shuvajitgupta <shuvajitgupta123@gmail.com>
2020-02-07 18:46:20 +0530

    updated

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+// const app = express();
+// const http = require("http").createServer(app);
+// const path = require("path");
+// const io = require("socket.io")(http);
+// const PORT = process.env.PORT || 81;
+
+// app.use(express.static(path.join(__dirname, "/index.html")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// io.on("connection", socket => {
+//   socket.on("message", msg => {
+//     console.log(msg);
+//   });
+// });
+
+// http.listen(PORT);
+// import * as express from "express";
+// import * as http from "http";
+// import * as WebSocket from "ws";
+
 const app = express();
-const http = require("http").createServer(app);
-const path = require("path");
-const io = require("socket.io")(http);
-const PORT = process.env.PORT || 81;
 
-app.use(express.static(path.join(__dirname, "/index.html")));
+//initialize a simple http server
+const server = http.createServer(app);
 
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
 
-io.on("connection", socket => {
-  socket.on("message", msg => {
-    console.log(msg);
+wss.on("connection", ws => {
+  //connection is up, let's add a simple simple event
+  ws.on("message", message => {
+    //log the received message and send it back to the client
+    console.log("received: %s", message);
+    ws.send(`Hello, you sent -> ${message}`);
   });
+
+  //send immediatly a feedback to the incoming connection
+  ws.send("Hi there, I am a WebSocket server");
 });
 
-http.listen(PORT);
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+  console.log(`Server started on port ${server.address().port} :)`);
+});
