560bd771c546ffce1d9ba5aa4d7d3633ccb726fd
shuvajitgupta <shuvajitgupta123@gmail.com>
2020-02-07 17:09:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+const path = require("path");
+const PORT = process.env.PORT || 81;
+
+app.use(express.static(path.join(__dirname, "index.html")));
+
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "index.html");
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", msg => {
+    console.log(msg);
+  });
+});
+
+http.listen(PORT);
