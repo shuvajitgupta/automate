39ed59b47b29fb82143f11a56889c6feb85272d7
shuvajitgupta <shuvajitgupta123@gmail.com>
2020-02-07 18:07:02 +0530

    updated

--- index.js
+++ index.js
@@ -1,75 +1,20 @@
-// const express = require("express");
-// var app = require("express")();
-// var http = require("http").createServer(app);
-// var io = require("socket.io")(http);
-// const path = require("path");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
 const PORT = process.env.PORT || 81;
 
-// app.use(express.static(path.join(__dirname, "/index.html")));
+app.use(express.static(path.join(__dirname, "/index.html")));
 
-// app.get("*", function(req, res) {
-//   res.sendFile(__dirname + "/index.html");
-// });
-
-// io.on("connection", function(socket) {
-//   socket.on("chat message", msg => {
-//     console.log(msg);
-//   });
-// });
-
-// http.listen(PORT);
-
-var WebSocketServer = require("websocket").server;
-var http = require("http");
-
-var server = http.createServer(function(request, response) {
-  console.log(new Date() + " Received request for " + request.url);
-  response.writeHead(404);
-  response.end();
-});
-server.listen(PORT);
-
-wsServer = new WebSocketServer({
-  httpServer: server,
-  // You should not use autoAcceptConnections for production
-  // applications, as it defeats all standard cross-origin protection
-  // facilities built into the protocol and the browser.  You should
-  // *always* verify the connection's origin and decide whether or not
-  // to accept it.
-  autoAcceptConnections: false
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
-
-wsServer.on("request", function(request) {
-  if (!originIsAllowed(request.origin)) {
-    // Make sure we only accept requests from an allowed origin
-    request.reject();
-    console.log(
-      new Date() + " Connection from origin " + request.origin + " rejected."
-    );
-    return;
-  }
-
-  var connection = request.accept("echo-protocol", request.origin);
-  console.log(new Date() + " Connection accepted.");
-  connection.on("message", function(message) {
-    if (message.type === "utf8") {
-      console.log("Received Message: " + message.utf8Data);
-      connection.sendUTF(message.utf8Data);
-    } else if (message.type === "binary") {
-      console.log(
-        "Received Binary Message of " + message.binaryData.length + " bytes"
-      );
-      connection.sendBytes(message.binaryData);
-    }
-  });
-  connection.on("close", function(reasonCode, description) {
-    console.log(
-      new Date() + " Peer " + connection.remoteAddress + " disconnected."
-    );
+io.on("connection", socket => {
+  socket.on("chat message", msg => {
+    console.log(msg);
   });
 });
+
+http.listen(PORT);
